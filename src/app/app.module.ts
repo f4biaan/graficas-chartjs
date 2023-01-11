84e306b42c019aa53c0e1951e957dbc0831b9841
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2023-01-11 05:05:10 -0500

    ejemplo de graficas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BarrasComponent } from './graficas/barras/barras.component';
+import { DonaComponent } from './graficas/dona/dona.component';
+import { LineComponent } from './graficas/line/line.component';
+import { BarrasDoblesComponent } from './graficas/barras-dobles/barras-dobles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BarrasComponent,
+    DonaComponent,
+    LineComponent,
+    BarrasDoblesComponent
   ],
   imports: [
     BrowserModule,
